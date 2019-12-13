cffdac9901c5936a69f7c578bcef146b14bf12db
Tyler <gasperlint@gmail.com>
2019-12-12 21:58:46 -0800

    add ability to navigate between tabs and set up stack nav

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {createStackNavigator} from 'react-navigation-stack'
 import AuthScreen from './screens/AuthScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
 import {createAppContainer} from 'react-navigation'
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import ReviewScreen from './screens/ReviewScreen'
 
 const MainNavigator = createBottomTabNavigator({
   welcome: {screen: WelcomeScreen},
-  auth: {screen: AuthScreen}
+  auth: {screen: AuthScreen},
+  main: {
+    screen: createBottomTabNavigator({
+      map: {screen: MapScreen},
+      deck: {screen: DeckScreen},
+      review: {
+        screen: createStackNavigator({
+          review: {screen: ReviewScreen},
+          settings: {screen: SettingsScreen}
+        })
+      }
+    })
+  }
 })
 
 class App extends React.Component {
