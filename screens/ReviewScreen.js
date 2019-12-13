6a7667d02875b5c7853c42a09ac45d582efc47e8
Tyler <gasperlint@gmail.com>
2019-12-12 22:26:48 -0800

    add review jobs header

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 
 class ReviewScreen extends React.Component{
+    static navigationOptions = {
+        //these are properties of the create stack navigatior within app.js
+           title: 'Review Jobs'
+/*            header: () => {
+/*                return {
+                   //more properties of stack nav
+                   right: <Text>Go Right Do Right</Text>
+             //  } */
+           //} 
+
+    }
+
     render(){
         return(
             <View>
