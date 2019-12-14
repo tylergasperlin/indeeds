a74638702672651456b63c3df4a24eebc1b20517
Tyler <gasperlint@gmail.com>
2019-12-14 10:17:09 -0800

    wire up action creator

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -6,7 +6,20 @@ import {
 //method for persistence in redux store
 //asyncStorage.setitem('fbtoken, token)
 //asyncstorage.getitem('fbtoekn)
-export const facebookLogin = () => {
-    
+
+//redux thunk works by returnig a function from our action creators 
+//then thunk calls the function and passes in the dispatch function
+//which allows us to dispatch a function at any time
+export const facebookLogin = () => async dispatch => {
+    let token = await AsyncStorage.getItem('fb_token')
+    if(token) {
+        //dispatch action saying fb login is done
+    } else {
+        //start fb login 
+
+    }
 
 }
+
+
+
