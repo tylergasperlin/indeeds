c23f92778da3ce006694e9b4fc1989c121d53354
Tyler <gasperlint@gmail.com>
2019-12-15 20:55:33 -0800

    add facebook login

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,10 +1,11 @@
 import {AsyncStorage} from 'react-native'
-import {Facebook} from 'expo'
+import * as Facebook from 'expo-facebook'
 import {
     FACEBOOK_LOGIN_SUCCESS,
     FACEBOOK_LOGIN_FAIL
 } from './types'
 
+const appId = '565826070869566'
 //method for persistence in redux store
 //asyncStorage.setitem('fbtoken, token)
 //asyncstorage.getitem('fbtoekn)
@@ -17,24 +18,27 @@ export const facebookLogin = () => async dispatch => {
     console.log('facebook log')
     if(token) {
         console.log('facebook log token')
-
         //dispatch action saying fb login is done
         dispatch({type: FACEBOOK_LOGIN_SUCCESS, payload: token })
     } else {
         console.log('facebook log token2')
         //console.log(type)
         //start fb login 
-        doFacebookLogin()
+        doFacebookLogin(dispatch)
     }
 }
 
 
 const doFacebookLogin = async dispatch => {
+    await Facebook.initializeAsync(appId)
+
     console.log('dispatch dofacebook')
-    let {result, token} = await Facebook.logInWithReadPermissionsAsync('565826070869566', {
+    let {type, token} = await Facebook.logInWithReadPermissionsAsync(appId, {
         permissions: ['public_profile']
     });
-
+    console.log('type return')
+    console.log(type)
+    //this runs if we get a type back of 'cancel' from facebook
     if (type=== 'cancel') {
         //WE GET ACCESS TO DISPATCH FROM THUNK
         //THIS IS A HELPER FUNCTION SO WE USE have to pass dispatch as a param
