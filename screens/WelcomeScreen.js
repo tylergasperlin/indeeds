6bb8d11fafe7955e47c47b64dc5c74fc45c224bd
Tyler <gasperlint@gmail.com>
2019-12-18 11:29:55 -0800

    add splash screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, AsyncStorage} from 'react-native'
 import Slides from '../components/Slides'
+import {AppLoading} from 'expo'
+import _ from 'lodash'
 
 
 const SLIDE_DATA = [
@@ -11,6 +13,26 @@ const SLIDE_DATA = [
 
 
 class WelcomeScreen extends React.Component{
+    state = {token: null}
+
+    constructor(props) {
+        super(props);
+
+        this.setToken()
+    }
+
+    async setToken(){
+        let token = await AsyncStorage.getItem('fb_token');
+
+        if(token){
+            this.props.navigation.navigate('map')
+            this.setState({token})
+        } else{
+            this.setState({token:false})
+        }
+    }
+
+
     onSlidesComplete = (navigation) => {
         //we get access to navigate function from react navigation library
         //since we render welcome screen with navigation we get a prop of navigation passed to our components
@@ -18,6 +40,9 @@ class WelcomeScreen extends React.Component{
     }
 
     render(){
+        if(_.isNull(this.state.token)){
+            return <AppLoading/>
+        }
         return(
             <View style={{flex:1}}>
                 <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
