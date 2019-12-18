fc6a9c83dbeb7f3ada434340db4d711bf35f3f26
Tyler <gasperlint@gmail.com>
2019-12-18 12:10:23 -0800

    add mapview

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, Dimensions} from 'react-native'
+import MapView from 'react-native-maps'
 
 class MapScreen extends React.Component{
-    render(){
-        return(
-            <View>
-                <Text>MapScreen</Text>
-                <Text>MapScreen</Text>
-                <Text>MapScreen</Text>
-                <Text>MapScreen</Text>
-                <Text>MapScreen</Text>
-                <Text>MapScreen</Text>
-            </View>
-        )
+    render() {
+        return (
+          <View style={styles.container}>
+            <MapView style={styles.mapStyle} />
+          </View>
+        );
+      }
     }
-
-}
-
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+      },
+    });
+    
 
 export default MapScreen;
