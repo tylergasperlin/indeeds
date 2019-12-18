c530f4cc9cc3eac3184d7897d8c4197c584afc99
Tyler <gasperlint@gmail.com>
2019-12-18 12:15:49 -0800

    add default location to map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,10 +3,20 @@ import {View, Text, StyleSheet, Dimensions} from 'react-native'
 import MapView from 'react-native-maps'
 
 class MapScreen extends React.Component{
+    state = {
+        region: {
+            longitude: -122,
+            latitude: 37,
+            longitudeDelta: 0.04,
+            latitudeDelta: 0.09
+        }
+    }
     render() {
         return (
           <View style={styles.container}>
-            <MapView style={styles.mapStyle} />
+            <MapView 
+                region={this.state.region}
+                style={styles.mapStyle} />
           </View>
         );
       }
